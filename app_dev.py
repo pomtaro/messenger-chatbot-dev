4cc9a4c2434480d16f6f230c33c70fc867c9e5e7
HigashiTakanori_LS <pomtaro0509@gmail.com>
2019-01-12 16:52:38 +0900

    4 commit

--- app_dev.py
+++ app_dev.py
@@ -5,11 +5,12 @@ from pymessenger2.bot import Bot
 # from pymessenger.bot_higashi import Bot_Higashi
 import os
 app = Flask(__name__)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 
-# ACCESS_TOKEN = 'EAAcCl48DZANsBAAVbqHZBeSlgCFFcp7Aeiu3cVFYDX2WOFhJOgsWT4Fx7Ld1rZBQCRQTo3kdSiMFxn4gD1zsgUMWaJF1DFctSSdXNtZCexKxLZBjQ8fse8BoaxKObFZBrbXuC18bmpa7maaR9Go5ySek2N3vdNf5qfcIiCw6dumgZDZD'
-# VERIFY_TOKEN = 'Verify_Token_Dev'
+#ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+#VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+
+ACCESS_TOKEN = 'EAAcCl48DZANsBAAVbqHZBeSlgCFFcp7Aeiu3cVFYDX2WOFhJOgsWT4Fx7Ld1rZBQCRQTo3kdSiMFxn4gD1zsgUMWaJF1DFctSSdXNtZCexKxLZBjQ8fse8BoaxKObFZBrbXuC18bmpa7maaR9Go5ySek2N3vdNf5qfcIiCw6dumgZDZD'
+VERIFY_TOKEN = 'Verify_Token_Dev'
 
 bot = Bot(ACCESS_TOKEN)
 # bot_higashi = Bot_Higashi(ACCESS_TOKEN)
